01da686a8658da89b8836df55bfb3012f6830e61
s3-odara <haruta@s3-odara.net>
2026-04-03 10:40:50 +0900

    update

--- home/dot-local/bin/pinentry-wrapper.sh
+++ home/dot-local/bin/pinentry-wrapper.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 
-PINENTRY_WOFI="/home/user/.local/bin/pinentry-wofi"
+PINENTRY_WOFI="/usr/local/bin/pinentry-wofi"
 PINENTRY_CURSES="/usr/bin/pinentry-curses"
 
 if [[ -n "${WAYLAND_DISPLAY:-}" || -n "${DISPLAY:-}" ]]; then
